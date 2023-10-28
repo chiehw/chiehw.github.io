5631359a1f2645dea0bd554231fe5a618b6b888f
chieh <todaymofish@gmail.com>
2023-10-28 12:08:44 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 git add . && git commit -m "update" && git push
 
-hexo g && echo "chieh.wang" >> public/CNAME
-# Deploy
-hexo clean && hexo deploy
+hexo g && echo "chieh.wang" >> public/CNAME && hexo deploy
